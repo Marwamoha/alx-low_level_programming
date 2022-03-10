3636bbbe55246031b22f8e6cdabf77a9e7992fb9
Marwamoha <marwahmohammed680@gmail.com>
2022-03-10 13:07:42 -0800

    c programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point 
  *
  * Return: Always 0 (success)
  */
-int  main(void){
+int  main(void)
+{
 	int a;
 	long int b;
 	long long int c;
@@ -15,7 +15,7 @@ int  main(void){
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 	return (0);
 }
 
